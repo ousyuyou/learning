3c8728bb050a35bacb1486f60ae366afa4790bdd
ousyuyou <ousyuyou@163.com>
2017-02-19 21:17:24 +0800

    update bagcalculate

--- src/algorithm/BagCalculate.java
+++ src/algorithm/BagCalculate.java
@@ -8,57 +8,22 @@ public class BagCalculate {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int m = 95000;
-//		int[] a = {9548,35120,6509,10090,32050,25160,45601,16500,9810,10000,55000};
-//		
+		int m = 95000;
+		int[] a = {9548,35120,6509,10090,32050,25160,45601,16500,9810,10000,55000};
+		
 //		int m = 89;
 //		int[] a = {7,11,13,5,2,10,50};
-//		
-//		calcBagEqual(m,a);
-	}
-	
-	/**
-	 * 
-	 * @param sum
-	 * @param array
-	 */
-	public static void calcBag(int m,int[] a){
-		int[][] c = new int[a.length][m+1];
 		
-		for(int j=1; j<=m; j++){
-			for(int i = 0; i < a.length;i++){
-				
-				if(j < a[i]){
-					continue;
-				}
-				
-				int x = i ==0 ? a[0] : c[i-1][j-a[i]] + a[i];
-				int y = i ==0 ? 0 : c[i-1][j];
-				
-				if(x>=j && y>=j){
-					System.out.println("x>=m && y>=m");
-					c[i][j] = Math.min(x, y);
-				} else if(x>=j && y<j){
-					c[i][j] = x;
-				} else if(x< j && y>=m){
-					c[i][j] = y;
-				}else if(x< j && y<j){
-					System.out.println("x<j && y<j");
-					c[i][j] = Math.max(x, y);
-				}
-
-			}
-		}
-		
-		System.out.println(c[a.length-1][m]);
-	}
+		calcBagOverMim(m,a,10000);
+	}	
 	
-	
-	public static void calcBagEqual(int m,int[] a){
+	public static void calcBagOverMim(int v,int[] a,int sumv){
+		int m = v + sumv;
 		int[][] c = new int[a.length][m+1];
 		int[][] d = new int[a.length][m+1];
 		
-		for(int j=1; j<=m; j++){
+		int j=1;
+		for(;j<=m; j++){
 			for(int i = 0; i < a.length;i++){
 				int y = i ==0 ? 0 : c[i-1][j];
 				
@@ -76,20 +41,23 @@ public class BagCalculate {
 					}
 				}
 			}
+			if(c[a.length-1][j] >= v){
+				break;
+			}
 		}
+		System.out.println(c[a.length-1][j]);
+		System.out.println("j= "+j);
 		
-		int s = m, count = 0;
+		int s = c[a.length-1][j];
 		StringBuffer strBuf = new StringBuffer();
 		
 		for(int i = a.length-1;i >= 0;i--){
 			strBuf.append(String.valueOf(d[i][s]));
 			if(d[i][s] == 1){
-				count++;
 				s = s - a[i];
 			}
 		}
 		
-		System.out.println(c[a.length-1][m]);
 		System.out.println(strBuf.reverse().toString());
 	}
 }
